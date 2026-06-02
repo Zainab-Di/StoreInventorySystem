f1fb0f4313070ee7484508ca25de6b9a6cdb9db7
marwa abdullah <marwasiwi04@gmail.com>
2026-06-02 11:20:27 +0200

    تمت إضافه واجهه إدارة المنتجات

--- StoreInventorySystem/Form1.cs
+++ StoreInventorySystem/Form1.cs
@@ -63,7 +63,7 @@ namespace StoreInventorySystem
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void panel2_Paint(object sender, PaintEventArgs e)
---
+++ StoreInventorySystem/ProductsForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace StoreInventorySystem
+{
+    partial class ProductsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button5 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(128, 120);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(216, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(128, 168);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(216, 23);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(128, 264);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(216, 23);
+            textBox3.TabIndex = 2;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(128, 72);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(216, 23);
+            textBox4.TabIndex = 3;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(128, 216);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(216, 23);
+            textBox5.TabIndex = 4;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(112, 304);
+            button1.Name = "button1";
+            button1.Size = new Size(91, 39);
+            button1.TabIndex = 5;
+            button1.Text = "حفظ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(288, 304);
+            button2.Name = "button2";
+            button2.Size = new Size(88, 39);
+            button2.TabIndex = 6;
+            button2.Text = "حذف";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(112, 368);
+            button3.Name = "button3";
+            button3.Size = new Size(96, 39);
+            button3.TabIndex = 7;
+            button3.Text = "تعديل";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(288, 368);
+            button4.Name = "button4";
+            button4.Size = new Size(88, 40);
+            button4.TabIndex = 8;
+            button4.Text = "تنظيف";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(408, 64);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(320, 248);
+            dataGridView1.TabIndex = 9;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 80);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 10;
+            label1.Text = "كود المنتج";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(48, 128);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 11;
+            label2.Text = "اسم المنتج";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(72, 176);
+            label3.Name = "label3";
+            label3.Size = new Size(30, 15);
+            label3.TabIndex = 12;
+            label3.Text = "الفئة";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(32, 224);
+            label4.Name = "label4";
+            label4.Size = new Size(81, 15);
+            label4.TabIndex = 13;
+            label4.Text = "الكمية الابتدائية";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(72, 272);
+            label5.Name = "label5";
+            label5.Size = new Size(36, 15);
+            label5.TabIndex = 14;
+            label5.Text = "السعر";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(24, 16);
+            button5.Name = "button5";
+            button5.Size = new Size(144, 23);
+            button5.TabIndex = 15;
+            button5.Text = "العودة للصفحة الرئيسية";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // ProductsForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "ProductsForm";
+            Text = "ProductsForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button5;
+    }
+}
